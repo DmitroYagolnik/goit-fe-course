38ecb3619f0ee26a3d36673f205dadf454b7d366
Dmitro Yagolnik <dima_yagolnik@ukr.net>
2019-05-17 22:21:36 +0300

    fixed last task

--- js/module-05/script.js
+++ js/module-05/script.js
@@ -166,7 +166,8 @@ console.log(getTotalBalance(users)); // 20916
  * Массив имен всех пользователей у которых есть друг с указанным именем
  */
 const getUsersByFriend = (arr, name) => arr
-    .filter(user => user.friends.includes(name));
+    .filter(user => user.friends.includes(name))
+    .map(user => user.name);
 
 console.log(getUsersByFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
 console.log(getUsersByFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
